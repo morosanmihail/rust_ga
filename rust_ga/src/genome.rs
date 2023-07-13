9337a5d17cdc606dfd40d4cd6d8fac68e0633846
Mihail Morosan <morosanmihail@outlook.com>
2023-07-13 19:10:11 +0100

    Make stuff more generic.

--- rust_ga/src/genome.rs
+++ rust_ga/src/genome.rs
@@ -1,57 +1,31 @@
 use rand::{Rng, seq::SliceRandom};
 
-const MAX_VALUE: i64 = 256;
-const MIN_VALUE: i64 = -256;
-const MAX_LEN: usize = 10;
 const POP_SIZE: usize = 10;
 
-const ALLELE_MUT_CHANCE: i32 = 20;
 
 const POP_MUT_CHANCE: i32 = 10;
 const POP_CROSS_CHANCE: i32 = 20;
 
 #[derive(Debug, Default)]
-pub struct Genome {
-    pub data: Vec<i64>,
-    pub fitness: Option<i64>,
+pub struct Genome<T: Clone + Default + Copy> {
+    pub data: Vec<T>,
+    pub fitness: Option<T>,
 }
 
-impl Genome {
-    pub fn new() -> Genome {
-        let mut rng = rand::thread_rng();
-        let mut data: Vec<i64> = Vec::new();
-        for _ in [1..=rng.gen_range(1..=MAX_LEN)] {
-            data.push(rng.gen_range(MIN_VALUE..MAX_VALUE));
-        }
-        Genome { data: data, ..Default::default() }
-    }
-
-    pub fn mutate(&self) -> Genome {
-        let mut rng = rand::thread_rng();
-        let mut new_data = self.data.clone();
-        for (_, v) in new_data.iter_mut().enumerate() {
-            if rng.gen::<i32>() % 100 < ALLELE_MUT_CHANCE {
-                // This is a hard mutate, where a big change is made
-                // We could also add / subtract a small value instead
-                *v = rng.gen_range(MIN_VALUE..MAX_VALUE);
-            }
-        }
-        Genome { data: new_data, ..Default::default() }
-    }
-
-    pub fn crossover(&self, other: &Genome) -> Genome {
+impl<T: Clone + Default + Copy> Genome<T> {
+    pub fn crossover(&self, other: &Genome<T>) -> Genome<T> {
         let mut rng = rand::thread_rng();
 
         let len_a = self.data.len();
         let len_b = other.data.len();
 
         let amount = rng.gen_range(1..=len_b);
-        let new_slice : Vec<i64> = other.data.choose_multiple(&mut rng, amount).cloned().collect();
+        let new_slice : Vec<T> = other.data.choose_multiple(&mut rng, amount).cloned().collect();
 
         let start_a = rng.gen_range(0..len_a);
         let end_a = rng.gen_range(start_a..len_a);
 
-        let mut new_data: Vec<i64> = Vec::new();
+        let mut new_data: Vec<T> = Vec::new();
         let mut inserted = false;
         for (i, v) in self.data.iter().enumerate() {
             if i < start_a || i > end_a {
@@ -74,22 +48,31 @@ pub trait Fitness {
     fn calculate_fitness(&mut self) -> Option<i64>;
 }
 
-#[derive(Debug)]
-pub struct Population {
-    pub members: Vec<Genome>,
+pub trait Mutate {
+    fn mutate(&self) -> Self;
 }
 
-impl Population {
-    pub fn new() -> Population {
-        let mut members: Vec<Genome> = Vec::new();
+pub trait Generate {
+    fn generate() -> Self;
+}
+
+#[derive(Debug)]
+pub struct Population<T: Generate + Mutate + Fitness> {
+    pub members: Vec<T>,
+}
+
+impl<T: Generate + Mutate + Fitness> Population<T> {
+    pub fn new() -> Population<T> {
+        let mut members: Vec<T> = Vec::new();
         for _ in 1..=POP_SIZE {
-            members.push(Genome::new());
+            // members.push(Genome::new());
+            members.push(T::generate());
         }
         Population { members: members }
     }
 
     pub fn tick(&mut self) {
-        let new_pop : Vec<Genome> = Vec::new();
+        let new_pop : Vec<T> = Vec::new();
         // Select biased towards top performers
         // ..this needs running calculate_fitness on all
         // Then randomly choose whether to crossover between chosen, or mutate
